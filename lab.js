d6af9827692c98bb63ae4f6f3646f82caf538de9
Mehdi Safari <sa.mehdisafari@gmail.com>
2021-12-15 13:43:18 -0800

    done with problem 4

--- lab.js
+++ lab.js
@@ -6,7 +6,6 @@ var lovesCode = true;
    console.log("Coding has it's challenges")
  }
 
-
 // Problem 2
 var amysAge = 29
 var brittanisAge = 34
@@ -23,7 +22,6 @@ if (amysAge > brittanisAge) {
   console.log(`${name1} and ${name2} are the same age`)
 }
 
-
 // Problem 3
 if (amysBirthYear === brittanisBirthYear) {
   console.log(`${name1} and ${name2} are born on the same year`) 
@@ -32,17 +30,23 @@ if (amysBirthYear === brittanisBirthYear) {
 }
 
 
-////////// PROBLEM 4 //////////
-
-let temperature = 55
-let rain = true
-
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
 */
 
-//CODE HERE
+// Problem 4
+let temperature = 55
+let rain = true
+
+if (temperature >= 80 && rain === true) {
+  console.log('You should wear a t-shirt and take an umbrella') 
+} else if ( 60 <= temperature <= 80 && rain === true) {
+  console.log('You should wear a rain jacket') 
+} else if (temperature >= 60) {
+  console.log('you should just chill')
+}
+
 
 
 ////////// PROBLEM 5 //////////
